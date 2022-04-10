0169cfe0af64382928178f5f5306f9ba58b03e97
Syed Dayim Shah <90561337+syedDayim@users.noreply.github.com>
2022-04-10 16:02:06 +0530

    Add files via upload

---
+++ First Sunday/Cprog_1.c
@@ -0,0 +1,75 @@
+//user___text_editor____compiler(GCC/G++)_____OS
+#include<stdio.h> //standard input output
+main(){
+    // printf("Hello, World");
+    // 4 varible
+    // integer (int){data type} , float (float){floating numbers}, double(double), character (char)
+    // staticlly typed language
+
+    // int x;
+    // float y; // 22.9999 ___23.000
+    // double z; // 25.999  ____25.999
+    // char a;  a-z, A-Z, !@#$%^&*()_+=-, 122424...
+
+
+    int num1; // declaration
+    num1 = 1000000; // initialization
+
+
+    int num2 = 10; // declaration & initialization
+
+    // valid  :  _num1,   num, NUM , Num, first_number, firstNumber,
+    // invalid : 1num, $num, first number,
+
+
+
+    // format specifiers:
+    // int   ---> %d
+    // float ---> %f
+    // double ---> %df
+    // char -----> %c
+
+
+
+   // & (amphersand) refelectes address
+
+
+    // The number you entered is 5
+    printf("The number you entered is %d\n", num1);
+
+
+    // The numbers you entered is 100000 and 10
+
+    printf("The numbers you entered is %d and %d \n",num1, num2   );
+
+
+
+
+    int num1 = 25;
+    int num2 = 100;
+    // +, -, /, *, %
+    
+    // int num3;
+    // num3 = num1 + num2;
+    // printf("%d \n", num3);
+    // printf("%d", num1 - num2);
+    
+    
+    //   / ----> returns quotient
+    //   % ----> remainder
+
+
+    printf("%d \n",  8 / 2     );  
+    printf("%d ",    3 % 2     );
+    
+    
+    
+    // varaible,
+    // printf(),
+    // how to name a varaible,
+    // format specifiers,
+    // arthemitic operations,
+
+
+
+}
