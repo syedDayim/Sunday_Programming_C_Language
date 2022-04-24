28f1ac53765a3099721f4959058e240d768c7b57
Syed Dayim Shah <90561337+syedDayim@users.noreply.github.com>
2022-04-24 16:00:12 +0530

    Add files via upload

---
+++ Logic_Building_Questions_Sunday3rd.c
@@ -0,0 +1,239 @@
+// Q: Print the sum of two numbers on console using only one print statement
+
+
+// #include<stdio.h>
+
+// int main(){
+
+
+// 	// The sum of 2 and 3 is 5
+
+// 	int x, y, z;
+
+// 	x = 2;
+// 	y = 3;
+// 	z = x + y;
+
+// 	printf("The sum of %d and %d is %d", x, y, z  );
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Q: Get a character from user and print its ASCII value
+
+	// A-Z a-z 1-0 !@#$$%^&*() :
+
+
+
+// #include<stdio.h>
+
+// int main(){
+
+// 	char ch;
+
+
+// 	printf("Enter a Character \n");
+	
+// 	scanf("%c", &ch);
+
+// 	printf("The ASCII value of %c is %d", ch, ch);
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Q: WAP to calculate the area of a circle
+	// pi * (r)square
+
+// #include<stdio.h>
+
+// int main(){
+
+// 	float pi = 3.14;
+// 	float r; 
+// 	float area;
+
+
+
+// 	printf("Enter Radius\n");
+
+
+// 	scanf("%f" , &r);
+
+
+// 	area = pi * r * r;
+
+
+
+// 	printf("The area of the circle with Radius %f is %f", r,  area );
+
+// }
+
+
+
+
+
+// Q: WAP to calculate the average of three numbers
+
+// #include<stdio.h>
+
+
+// int main(){
+
+// 	int a, b, c;
+
+// 	float avg;
+
+
+// 	printf("Enter Numbers\n");
+
+// 	scanf("%d %d %d", &a, &b, &c );
+
+
+
+// 	avg = (a + b + c) / 3;
+
+// //  the agverage of a b c is avg
+// 	printf("The average %d and %d and %d is %f", a, b, c, avg);
+
+
+
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// Q: WAP to print only last digit of a number 
+
+
+	
+
+// 	    // 10 % 1234 
+// #include<stdio.h>
+
+
+// main(){
+
+// 	int number;
+// 	int right_most_digit;
+
+// 	printf("Enter a number \n");
+
+// 	scanf("%d", &number);
+
+
+// 	right_most_digit =  10 % number ;
+
+
+
+
+
+// 	printf("The right_most_digit of %d is %d",number, right_most_digit);
+
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Q: Swap the number stored in variables
+
+
+// #include<stdio.h>
+// int main(){
+
+// 	int x; // 20
+// 	int y; // 20
+
+// 	int temporary; // 10
+
+
+// 	printf("Enter the values of X and Y\n");
+// 	scanf("%d %d", &x, &y);
+
+// 	printf("values of X and Y before swapping %d %d\n", x, y);
+
+
+// 	temporary = x;
+
+// 	x = y;
+
+// 	y = temporary;
+
+
+// 	printf("values of X and Y after swapping %d %d\n", x, y);
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
